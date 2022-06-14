8f307a972b761fbb0e0d34af11d7156439fdb92c
rhunold <raphael.hunold@gmail.com>
2022-06-14 17:15:30 +0200

    Ajout fonctionnement tour 2 et +

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,3 +1,6 @@
+import time
+import random
+
 from models.player import Player
 from models.match import Match
 from models.tour import Tour
@@ -9,7 +12,7 @@ from database import *
 
 class CreateTournament:
     
-    NUMBER_PARTICIPANTS = 4
+    NUMBER_PARTICIPANTS = 8
 
     # Instanciation Tournois
 
@@ -18,21 +21,18 @@ class CreateTournament:
     # match = Match()
     player = Player()
 
-    # tournament.name = "Grand tournoi rentrée 2022"
-    # tournament.description = "Welcome !!!"
-    # tournament.start_date = get_timestamp()
-    # tournament.end_date = ""
-    # tournament.list_participants = []
-    # tournament.tour_number = 4    
-    # tournament.list_tours = [] 
-    # tournament.time_control = ""  
-    # tournament.location = ""    
+    tournament.name = "Grand tournoi rentrée 2022"
+    tournament.description = "Welcome !!!"
+    tournament.start_date = get_timestamp()
+    tournament.end_date = ""
+    tournament.list_participants = []
+    tournament.tour_number = 4    
+    tournament.list_tours = [] 
+    tournament.time_control = ""  
+    tournament.location = ""    
     # time.sleep(2.5)   
     
     # print(tournament)
-    
-
-    
 
     
     registered_participants = len(tournament.list_participants)
@@ -40,8 +40,10 @@ class CreateTournament:
   
     # On demande au directeur de choisir 8 participants parmi les joueurs crées au préalable.
     while registered_participants <= NUMBER_PARTICIPANTS:
-
-        player_id = int(input("Ajouter un joueur avec son identifiant : "))
+        
+        player_id = random.randrange(10) # Ajout automatique d'un joueur ayant id de 0 à 10
+        # player_id = int(input("Ajouter un joueur avec son identifiant : "))
+        
         tournament.add_player(player_id)
         
         print(f"Il y a {str(len(tournament.list_participants))} joueurs inscrits au tournoi. Il en manque {str(NUMBER_PARTICIPANTS - len(tournament.list_participants))}")        
@@ -53,94 +55,106 @@ class CreateTournament:
     
     
     
-    print(f"Ok pour les joueurs. On va générer le 1er tour.")
+    print(f"Ok pour les joueurs.")
     
+    while tournament.tour_number != len(tournament.list_tours): 
+
+        # On génère le 1er tour si aucun tour n'est dans la liste
+        if tournament.list_tours == []:
+            
+            print(f"1er tour")
+            
+            tour = Tour()
+
+            sort_participants_by_rank = sorted(tournament.list_participants, key=lambda x: x.rank, reverse=False)
+            
+            # Vérifier que l'on a bien un nombre pair de joueur.
+            
+            #initialize the middle index with the length of first half
+            middle=int(len(tournament.list_participants)/2)
+
+            #Split the list from starting index upto middle index in first half
+            first_players=sort_participants_by_rank[:middle]
+
+            #Split the list from middle index index upto the last index in second half
+            sec_players=sort_participants_by_rank[middle:]
+
+        else :
+            print(f"{str(len(tournament.list_tours)+1)}ème tour")
+            tour = Tour()               
+            
+            #  triez tous les joueurs en fonction de leur nombre total de points. Si plusieurs joueurs ont le même nombre de points, triez-les en fonction de leur rang.
+            sort_participants_by_score_and_rank = sorted(tournament.list_participants, key=lambda x: (-x.player_score, x.rank))
+            
+            #Split the list from starting index upto middle index in first half
+            first_players=sort_participants_by_score_and_rank[::2]
+
+            #Split the list from middle index index upto the last index in second half
+            sec_players=sort_participants_by_score_and_rank[1::2]            
 
 
-    # On génère le 1er tour si aucun tour n'est dans la liste
-    if tournament.list_tours == []:
-        
-        tour1 = Tour()
-
-        sort_participants_by_rank = sorted(tournament.list_participants, key=lambda x: x.rank, reverse=False)
-        
-        # Vérifier que l'on a bien un nombre pair de joueur.
-        
-        #initialize the middle index with the length of first half
-        middle=int(len(tournament.list_participants)/2)
-
-        #Split the list from starting index upto middle index in first half
-        first_players=sort_participants_by_rank[:middle]
-
-        #Split the list from middle index index upto the last index in second half
-        sec_players=sort_participants_by_rank[middle:]
-
-        for i, (first_player, sec_player) in enumerate(zip(first_players, sec_players)):
-            # print(repr(first_player), repr(sec_player)) 
+        allmatch = []
+        for first_player, sec_player in zip(first_players, sec_players):
             
             player1_id = first_player.player_id
             player2_id = sec_player.player_id
+                   
+            match = Match(player1_id, player2_id) 
             
-            # player1_total_score = first_player.player_score
-            # player2_total_score = sec_player.player_score            
+            # Je crée un deuxième match avec pair inversé pour pouvoir vérifier tous les matchs.
+            match2 = Match(player2_id, player1_id)                
+            
+            for match_done in allmatch:      
+                if match_done == match:
+                    print(f"Le match {match} a déjà été joué. On refait l'attribution du joueur 2")  
+                    # print(match_done, match)
+                    iteractir_player2_id = iter(sec_players)
+                    old_player2_id = next(iteractir_player2_id)
+                    player2_id = next(iteractir_player2_id)
+                    match = Match(player1_id, player2_id)
+                    match2 = Match(player2_id, player1_id)   
+                    break
+                else:
+                    continue
 
-            tour1.list_matchs.append(Match(player1_id, player2_id))
-
-
-        # On ajouter le 1er tour au tournois
-        tournament.list_tours.append(tour1)  
-
-              
+            
+            # J'ajoute les pairs symetrique à ma liste de tous les matchs
+            allmatch.append(match)    
+            allmatch.append(match2)    
+            
+            # On ajoute le match au tour
+            tour.list_matchs.append(match)
         
-        # On joue les match du tour 1 pour assigner le score du match
-        for match in tour1.list_matchs:
+            
+        # On joue les match du tour pour assigner le score du match
+        for match in tour.list_matchs:
+
             match.play_match()
             
-            # print(player1_total_score)
-            # print(match.player1_score)            
-            # player1_total_score += match.player1_score
-
-            # print(f"Le joueur avec l'id {player1_id} a {player1_total_score} points")            
-            
-        tour1._end_date_and_hour = get_timestamp()
-  
-
-
-        print(tournament.list_tours)
-        # print(tournament.list_participants)               
+            # Assignation des points obtenus au score des joueurs
+            for participant in tournament.list_participants:
+                if match.player1_id == participant.player_id:
+                    participant.player_score += match.player1_score
+                if match.player2_id == participant.player_id:
+                        participant.player_score += match.player2_score
+                
+                # print(f"le nouveau score de {participant.player_id} est {participant.player_score}")
         
-        if tour1:
-            print("On passe au tours 2 et suivants")
-            
-            # On passe au deuxième tour
-            # sort_participants_by_score = sorted(tournament.list_participants, key=lambda x: x.player_score, reverse=False)
-            
-            # print(sort_participants_by_score)            
-            
-            tour = Tour()
-            tournament.list_tours.append(tour)  
-            print(tournament.list_tours)
-            pass
+        time.sleep(1.5)      
+        tour._end_date_and_hour = get_timestamp()
 
-
-
-
- 
-
-
+        # On ajouter le tour au tournois
+        tournament.list_tours.append(tour)
         
-        """
-        Au prochain tour, triez tous les joueurs en fonction de leur nombre total de points. 
-        Si plusieurs joueurs ont le même nombre de points, triez-les en fonction de leur rang.
-        Associez le joueur 1 avec le joueur 2, le joueur 3 avec le joueur 4, et ainsi de suite. Si le joueur 1 a déjà joué contre le joueur 2, associez-le plutôt au joueur 3.
-        
-        Répétez les étapes jusqu'à ce que le tournoi soit terminé.
-          """          
+    print(tournament)
+    
+    # and the winner of the tournament is 
+    sort_participants_by_score = sorted(tournament.list_participants, key=lambda x: x.player_score, reverse=True)
+    print(f"Le gagnant est {sort_participants_by_score[0]}")
 
+    
         
-
-    # print(tournament)
-                 
+            
 
 
 class CreatePlayer:
