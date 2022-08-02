a9933992e60afbf38238eb4dea2c341093e58c1a
rhunold <raphael.hunold@gmail.com>
2022-08-02 15:07:37 +0200

    Cleanage code

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-
 from controllers.menu_manager import Home
 
 
-def main(): 
+def main():
     Home().display()
+    """Display the Home menu"""
 
-        
 
 if __name__ == "__main__":
     main()
- 
