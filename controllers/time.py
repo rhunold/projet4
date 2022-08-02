30da8650346505d00896d540c6dffbbfdc99bea3
rhunold <raphael.hunold@gmail.com>
2022-08-02 14:54:39 +0200

    Pep8

--- controllers/time.py
+++ controllers/time.py
@@ -1,12 +1,9 @@
 from datetime import datetime
 
 
-
 def get_timestamp():
     return datetime.now().strftime("%d-%m-%Y %H:%M:%S")
 
+
 def get_date():
     return datetime.now().strftime("%d-%m-%Y")
-
-
-
